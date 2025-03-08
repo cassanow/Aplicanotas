d45a0c67848b43e6b56150d7ba9492313930c31c
arthur <arthur@DESKTOP-TP4TC98>
2025-03-08 14:07:07 -0300

    Adicionar arquivos de projeto.

---
+++ Aplicanotas/Notas.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicanotas
+{
+    class Nota
+    {
+        private string titulo { get; set; }
+        private string conteudo { get; set; }
+
+        List<Nota> notas = new List<Nota>();
+        public void criarNotas()
+        {
+            
+
+            Console.WriteLine("Digite o título de sua nota:");
+            titulo = Console.ReadLine();
+
+           if (string.IsNullOrEmpty(titulo)) {
+                Console.WriteLine("Por favor, digite alguma coisa.");
+            }   
+
+            Console.WriteLine("Digite o conteúdo de sua nota:");
+            conteudo = Console.ReadLine();
+            if (string.IsNullOrEmpty(conteudo)) {
+                Console.WriteLine("Por favor, digite alguma coisa.");
+            }   
+            notas.Add(new Nota { titulo = titulo, conteudo = conteudo });
+            foreach (var nota in notas)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Título: " + nota.titulo);
+                Console.WriteLine("Conteúdo: " + nota.conteudo);
+            }       
+
+        }
+
+        public void editarNotas()
+        {
+            Console.WriteLine("Digite o título da nota que deseja editar:");
+            titulo = Console.ReadLine();
+            Console.WriteLine("Digite o novo conteúdo da nota:");
+            string novoConteudo = Console.ReadLine();
+            notas.Add(new Nota { titulo = titulo, conteudo = novoConteudo });
+            foreach (var nota in notas)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Título: " + nota.titulo);
+                Console.WriteLine("Conteúdo: " + nota.conteudo);
+            }   
+        }
+
+        public void excluirNotas()
+        {
+            Console.WriteLine("Digite o título da nota que deseja excluir:");
+            titulo = Console.ReadLine();
+            notas.RemoveAll(nota => nota.conteudo == titulo);
+            foreach (var nota in notas)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Título: " + nota.titulo);
+                Console.WriteLine("Conteúdo: " + nota.conteudo);
+            }
+        }       
+
+        public void listarNotas()
+        {
+            foreach (var nota in notas)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Título: " + nota.titulo);
+                Console.WriteLine("Conteúdo: " + nota.conteudo);
+                Console.WriteLine("");
+                Console.WriteLine("");
+            }
+        }   
+    }
+}
---
+++ Aplicanotas/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicanotas
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Nota nota = new Nota();
+
+            Console.WriteLine("Escolha uma opção:");
+            Console.WriteLine("1 - Criar nota\n2 - Editar nota\n3 - Excluir nota\n4 - Listar notas\n5 - Sair");
+            int Opcao = int.Parse(Console.ReadLine());
+            while (Opcao != 5)
+            {
+                switch (Opcao)
+                {
+                    case 1:
+                        nota.criarNotas();
+                        Console.WriteLine("Escolha uma opção:");
+                        Console.WriteLine("1 - Criar nota\n2 - Editar nota\n3 - Excluir nota\n4 - Listar notas\n5 - Sair");
+                        Opcao = int.Parse(Console.ReadLine());
+                        break;
+                    case 2:
+                        nota.editarNotas();
+                        Console.WriteLine("Escolha uma opção:");
+                        Console.WriteLine("1 - Criar nota\n2 - Editar nota\n3 - Excluir nota\n4 - Listar notas\n5 - Sair");
+                        Opcao = int.Parse(Console.ReadLine());
+                        break;
+                    case 3:
+                        nota.excluirNotas();
+                        Console.WriteLine("Escolha uma opção:");
+                        Console.WriteLine("1 - Criar nota\n2 - Editar nota\n3 - Excluir nota\n4 - Listar notas\n5 - Sair");
+                        Opcao = int.Parse(Console.ReadLine());
+                        break;
+                    case 4:
+                        nota.listarNotas();
+                        Console.WriteLine("Escolha uma opção:");
+                        Console.WriteLine("1 - Criar nota\n2 - Editar nota\n3 - Excluir nota\n4 - Listar notas\n5 - Sair");
+                        Opcao = int.Parse(Console.ReadLine());
+                        break;
+                    case 5:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida");
+                        Console.WriteLine("Escolha uma opção:");
+                        Console.WriteLine("1 - Criar nota\n2 - Editar nota\n3 - Excluir nota\n4 - Listar notas\n5 - Sair");
+                        Opcao = int.Parse(Console.ReadLine());
+                        break;
+                }
+            }
+        }
+    }
+}
